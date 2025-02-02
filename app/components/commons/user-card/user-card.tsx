dbdc74d2a692529f0d97a818a999f0e680e6e625
Rodrigo Weich <rodrigoweich@hotmail.com>
2025-02-02 20:01:21 -0300

    Corrigindo os Componentes na Landing Page

--- app/components/commons/user-card/user-card.tsx
+++ app/components/commons/user-card/user-card.tsx
@@ -1,4 +1,4 @@
-import {Github, Instagram, Linkedin, Twitter} from "lucide-react";
+import {Github, Instagram, Linkedin, Plus, Twitter} from "lucide-react";
 import EditSocialLinks from "./edit-social-links";
 import Button from "../../ui/button";
 import Link from "next/link";
@@ -13,8 +13,10 @@ export default async function UserCard({
                                            isOwner,
                                        }: {
     profileData?: ProfileData;
-    isOwner: boolean;
+    isOwner?: boolean;
 }) {
+    const icons = [Github, Instagram, Linkedin, Twitter, Plus];
+
     return (
         <div
             className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
@@ -23,7 +25,7 @@ export default async function UserCard({
                     src={
                         (await getDownloadURLFromPath(profileData?.imagePath)) || "/me.webp"
                     }
-                    alt="André Dev"
+                    alt="Profile image"
                     className="rounded-full object-cover w-full h-full"
                 />
             </div>
@@ -78,7 +80,15 @@ export default async function UserCard({
                             <Twitter/>
                         </Link>
                     )}
-
+                    {!profileData &&
+                        icons.map((Icon, index) => (
+                            <button
+                                key={index}
+                                className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"
+                            >
+                                <Icon/>
+                            </button>
+                        ))}
                     {isOwner && (
                         <EditSocialLinks socialMedias={profileData?.socialMedias}/>
                     )}
@@ -113,7 +123,11 @@ export default async function UserCard({
                             <Button className="w-full">{profileData.link3.title}</Button>
                         </Link>
                     )}
-
+                    {!profileData && (
+                        <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]">
+                            <Plus/>
+                        </button>
+                    )}
                     {isOwner && <AddCustomLink/>}
                 </div>
             </div>
--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,8 +1,8 @@
+import ProjectCard from "../commons/project-card";
+import TotalVisits from "../commons/total-visits";
 import UserCard from "../commons/user-card/user-card";
 import Button from "../ui/button";
 import TextInput from "../ui/text-input";
-import TotalVisits from "@/app/components/commons/total-visits";
-import ProjectCard from "@/app/components/commons/project-card";
 
 export default function Hero() {
     return (
@@ -28,13 +28,21 @@ export default function Hero() {
                 <div className="relative">
                     <UserCard/>
                     <div className="absolute -bottom-[7%] -right-[45%]">
-                        <TotalVisits/>
+                        <TotalVisits totalVisits={1342}/>
                     </div>
                     <div className="absolute top-[20%] -left-[45%] -z-10">
-                        <ProjectCard/>
+                        <ProjectCard
+                            name="Projeto 1"
+                            description="Descrição do projeto 1"
+                            img="project1.jpg"
+                        />
                     </div>
                     <div className="absolute -top-[5%] -left-[55%] -z-10">
-                        <ProjectCard/>
+                        <ProjectCard
+                            name="Projeto 2"
+                            description="Descrição do projeto 2"
+                            img="project2.jpg"
+                        />
                     </div>
                 </div>
             </div>
