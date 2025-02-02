05c9c5acc09c378f144643166a20848baff682d3
Rodrigo Weich <rodrigoweich@hotmail.com>
2025-02-02 11:23:51 -0300

    Página Criar Link

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/landing-page/header";
-import Hero from "./components/landing-page/hero";
-import Pricing from "./components/landing-page/pricing";
-import VideoExplanation from "./components/landing-page/video-explanation";
+import Header from "../components/landing-page/header";
+import Hero from "../components/landing-page/hero";
+import Pricing from "../components/landing-page/pricing";
+import VideoExplanation from "../components/landing-page/video-explanation";
 import FAQ from "@/app/components/landing-page/faq";
 
 export default function Home() {
