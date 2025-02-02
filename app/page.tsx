a33b4efdae0c3b9f1a9c2097bf020370e892c07a
Rodrigo Weich <rodrigoweich@hotmail.com>
2025-02-02 11:17:36 -0300

    Criando seções de Video e Pricing

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Hero from "@/app/components/landing-page/hero";
-import Header from "@/app/components/landing-page/header";
+import Header from "./components/landing-page/header";
+import Hero from "./components/landing-page/hero";
+import Pricing from "./components/landing-page/pricing";
+import VideoExplanation from "./components/landing-page/video-explanation";
 
 export default function Home() {
     return (
         <div className="max-w-7xl mx-auto">
             <Header/>
             <Hero/>
+            <VideoExplanation/>
+            <Pricing/>
 
-            {/*
-      <VideoExplanation />
-      <Pricing />
-      <FAQ /> */}
+            {/* <FAQ /> */}
         </div>
     );
 }
