79f71ad74db2fc851994cfd9ad496d97734e8c5b
Rodrigo Weich <rodrigoweich@hotmail.com>
2025-02-02 19:58:59 -0300

    Adicionando Page Views e Cliques

--- app/(pages)/[profileId]/page.tsx
+++ app/(pages)/[profileId]/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import {notFound} from "next/navigation";
 import NewProject from "./new-project";
 import {getDownloadURLFromPath} from "@/app/lib/firebase";
+import {increaseProfileVisits} from "@/app/actions/increase-profile-visits";
 
 export default async function ProfilePage({
                                               params,
@@ -19,15 +20,15 @@ export default async function ProfilePage({
 
     if (!profileData) return notFound();
 
-    // TODO: get projects
-
     const projects = await getProfileProjects(profileId);
 
     const session = await auth();
 
     const isOwner = profileData.userId === session?.user?.id;
 
-    // TODO: Adicionar page view
+    if (!isOwner) {
+        await increaseProfileVisits(profileId);
+    }
 
     // Se o usuario não estiver mais no trial, nao deixar ver o projeto. Redirecionar para upgrade
 
@@ -56,9 +57,11 @@ export default async function ProfilePage({
                 ))}
                 {isOwner && <NewProject profileId={profileId}/>}
             </div>
-            <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
-                <TotalVisits/>
-            </div>
+            {isOwner && (
+                <div className="absolute bottom-4 right-0 left-0 w-min mx-auto">
+                    <TotalVisits totalVisits={profileData.totalVisits}/>
+                </div>
+            )}
         </div>
     );
 }
--- app/actions/create-project.ts
+++ app/actions/create-project.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import {Timestamp} from "firebase-admin/firestore";
 import {auth} from "../lib/auth";
 import {db, storage} from "../lib/firebase";
@@ -7,24 +8,30 @@ import {randomUUID} from "crypto";
 export async function createProject(formData: FormData) {
     const session = await auth();
     if (!session) return;
+
     const profileId = formData.get("profileId") as string;
     const projectName = formData.get("projectName") as string;
     const projectDescription = formData.get("projectDescription") as string;
     const projectUrl = formData.get("projectUrl") as string;
     const file = formData.get("file") as File;
+
     const generatedId = randomUUID();
+
     const storageRef = storage.file(`project-images/${profileId}/${generatedId}`);
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     await storageRef.save(buffer);
+
     const imagePath = storageRef.name;
+
     try {
         await db
-            .collection("projects")
+            .collection("profiles")
             .doc(profileId)
             .collection("projects")
-            .doc()
+            .doc(generatedId)
             .set({
+                id: generatedId,
                 userId: session.user?.id,
                 projectName,
                 projectDescription,
@@ -32,6 +39,7 @@ export async function createProject(formData: FormData) {
                 imagePath,
                 createdAt: Timestamp.now().toMillis(),
             });
+
         return true;
     } catch (error) {
         console.error(error);
