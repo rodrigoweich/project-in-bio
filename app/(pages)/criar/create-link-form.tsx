221cbb023c1bbb24004ff3fbbc6b077b40cf2bb1
Rodrigo Weich <rodrigoweich@hotmail.com>
2025-02-03 10:51:17 -0300

    Ajustes no Fluxo do Usuário

--- app/(pages)/criar/create-link-form.tsx
+++ app/(pages)/criar/create-link-form.tsx
@@ -5,12 +5,17 @@ import {verifyLink} from "@/app/actions/verify-link";
 import Button from "@/app/components/ui/button";
 import TextInput from "@/app/components/ui/text-input";
 import {sanitizeLink} from "@/app/lib/utils";
-import {useRouter} from "next/navigation";
+import {useRouter, useSearchParams} from "next/navigation";
 import {useState} from "react";
 
 export default function CreateLinkForm() {
     const router = useRouter();
-    const [link, setLink] = useState("");
+
+    const searchParams = useSearchParams();
+
+    const [link, setLink] = useState(
+        sanitizeLink(searchParams.get("link") || "")
+    );
     const [error, setError] = useState("");
 
     function handleLinkChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -20,15 +25,21 @@ export default function CreateLinkForm() {
 
     async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
+
         // Quando o usuario nao escreve um link
         if (link.length === 0) return setError("Escolha um link primeiro :)");
+
         // Quando o usuario escolhe um link ja existente
         const isLinkTaken = await verifyLink(link);
+
         if (isLinkTaken) return setError("Desculpe, esse link já está em uso.");
+
         // Criar o perfil
         const isLinkCreated = await createLink(link);
+
         if (!isLinkCreated)
             return setError("Erro ao criar o perfil. Tente novamente.");
+
         router.push(`/${link}`);
     }
 
