9d6e74e9896b0815ec4c9d5be955653785d4cb87
Rodrigo Weich <rodrigoweich@hotmail.com>
2025-02-03 09:27:32 -0300

    Criando Checkout Stripe

---
+++ app/(pages)/[profileId]/upgrade/plan-buttons.tsx
@@ -0,0 +1,24 @@
+"use client";
+import Button from "@/app/components/ui/button";
+import {useStripe} from "@/app/hooks/useStripe";
+import {useParams} from "next/navigation";
+
+export default function PlanButtons() {
+    const {profileId} = useParams();
+    const {createStripeCheckout} = useStripe();
+    return (
+        <div className="flex gap-4">
+            <Button
+                onClick={() =>
+                    createStripeCheckout({
+                        metadata: {profileId},
+                        isSubscription: true,
+                    })
+                }
+            >
+                R$ 9,90 / mês
+            </Button>
+            <Button>R$ 99,90 Vitalício</Button>
+        </div>
+    );
+}
